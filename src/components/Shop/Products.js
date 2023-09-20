49f2b3af0eb9e9abbecfda5e05bdab07fdf1ccd1
ferelie <abdallah.yasser0501@gmail.com>
2023-09-20 20:36:39 +0300

    deploy

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -4,17 +4,42 @@ import classes from './Products.module.css';
 const DUMMY_PRODUCTS = [
   {
     id: 'p1',
-    price: 6,
-    title: 'My First Book',
-    description: 'The first book I ever wrote',
+    price: 19.99,
+    title: 'The Last of Us Remastered (PS4)',
+    description: 'Experience an emotional story in a post-apocalyptic world.',
   },
   {
     id: 'p2',
-    price: 5,
-    title: 'My Second Book',
-    description: 'The second book I ever wrote',
+    price: 29.99,
+    title: 'Uncharted 4: A Thief\'s End (PS4)',
+    description: 'Join Nathan Drake on his final adventure in this action-packed game.',
+  },
+  {
+    id: 'p3',
+    price: 9.99,
+    title: 'Bloodborne (PS4)',
+    description: 'Face challenging enemies and uncover dark secrets in Yharnam.',
+  },
+  {
+    id: 'p4',
+    price: 49.99,
+    title: 'God of War (PS4)',
+    description: 'Embark on a mythological journey as Kratos and his son Atreus.',
+  },
+  {
+    id: 'p5',
+    price: 14.99,
+    title: 'Horizon Zero Dawn (PS4)',
+    description: 'Explore a breathtaking open world and battle robotic creatures.',
+  },
+  {
+    id: 'p6',
+    price: 39.99,
+    title: 'Marvel\'s Spider-Man (PS4)',
+    description: 'Swing through the streets of New York City as the friendly neighborhood Spider-Man.',
   },
 ];
+
 
 const Products = (props) => {
   return (
